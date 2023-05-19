d462dff08f24fdc43bac123604bacb574c564c4e
Mayank <72690358+m-ayan-k@users.noreply.github.com>
2023-05-20 01:23:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-
+//use your firebaseConfig
 const firebaseConfig = {
-    apiKey: "AIzaSyAUvt2J22y_mkA4hucyhTW8r5mRCwX7vgo",
-    authDomain: "botchat-17c0d.firebaseapp.com",
-    projectId: "botchat-17c0d",
-    storageBucket: "botchat-17c0d.appspot.com",
-    messagingSenderId: "822290990646",
-    appId: "1:822290990646:web:503c271177394325217a97"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
