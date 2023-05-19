f05e51c78e963a664237d9aa663ad6b1c3cb3598
m-ayan-k <mycoolfriends098@gmail.com>
2023-05-20 00:18:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useContext,Suspense  } from "react";
+// import Login from './Pages/Login';
+// import Home from './Pages/Home';
+import "./style.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
+const Home=React.lazy(()=>import("./Pages/Home"));
+const Register=React.lazy(()=>import("./Pages/Register"));
+const Login=React.lazy(()=>import("./Pages/Login"));
+
+
 
 function App() {
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"
+            exact
+            element={ 
+              <Suspense fallback = { <div> Please Wait... </div> } >
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              </Suspense>
+              
+            }
+          />
+        <Route path="register" element={
+          <Suspense fallback = { <div> Please Wait... </div> } >
+            <Register />
+          </Suspense>
+        }/>
+        <Route path="login" element={
+            <Suspense fallback = { <div> Please Wait... </div> } >
+              <Login />
+            </Suspense>
+          } 
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
